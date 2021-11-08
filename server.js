b567570965f551b4b9c13a395a49bc3359b87a92
hanielchang <haniellloyd@gmail.com>
2021-11-07 16:16:24 -0800

    submitting project for grade

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+
+// Turning on connection to db and server. The "sync" part means that Sequelize is
+// taking the models and connecting them to associated database tables. 
+// If it doesn't find a table, it'll create it for you!
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
